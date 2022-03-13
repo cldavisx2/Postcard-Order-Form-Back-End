a687110e3499e8a8768c946e58fa401f21d6d622
chris davis <cldavisx2@gmail.com>
2022-03-12 19:11:38 -0500

    added the route for image requests

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 app.get('/',(req,res) => {
     res.send('Things appear to be working just fine here...');
 })
 
-app.get('/get-thumbnail/:img',(req,res) => {
-    //we don't actually need this, we only need the link, which we will 
-    //already know from the get-imgs route
-    res.send(req.params.img);
+//need this so we can return the actual files when they are requested
+app.get('/images/thumbnails/:img',(req,res) => {
+    //should check if it exists, if not send an error
+    res.sendFile(`${__dirname}/images/thumbnails/${req.params.img}.jpg`);
+})
+app.get('/images/fullsize/:img',(req,res) => {
+    res.sendFile(`${__dirname}/images/fullsize/${req.params.img}.jpg`)
 })
 
 
